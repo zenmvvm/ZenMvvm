31bdf92c547e148dd1be6cc0ce045b1fa88a7fac
Zeebs <zeebsmail@gmail.com>
2020-09-22 11:19:24 +0200

    Port Tests

---
+++ src/ZenMvvm.Tests/Views/EmptyPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace ZenMvvm.Tests.Views
+{
+    public partial class EmptyPage : ContentPage
+    {
+        public EmptyPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
